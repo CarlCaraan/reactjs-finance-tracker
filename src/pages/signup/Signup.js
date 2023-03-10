36efacbe025aa2ff219fd06764d98031c4129d24
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-10 21:43:15 +0800

    3/10/23

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styles
 import styles from "./Signup.module.css";
 
 function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Sign Up</h2>
+
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
+  );
 }
 
 export default Signup;
