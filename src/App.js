8c755c75761e7d47f44e33575809c6c4d2314b64
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-10 20:29:34 +0800

    3/10/23

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        {/* Navigation */}
+        <Navbar />
+
         <Switch>
           <Route exact path="/">
             <Home />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // Styles
 import styles from "./Navbar.module.css";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>
+          <Link to="/">myMoney</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
