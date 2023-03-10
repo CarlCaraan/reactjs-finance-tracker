30c926aaaa481fdb895f0977337305a6727e2d58
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-10 21:16:57 +0800

    login template

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Styles
 import styles from "./Login.module.css";
 
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
+      <h2>Login</h2>
+
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
 }
 
 export default Login;
