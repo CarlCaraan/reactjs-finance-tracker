a3af882ca13d76dfffcf6b170769589ff64de9f3
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-11 21:50:40 +0800

    3/11/23

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoPAA5uz7nYGQrTNxipBNtn9SqwBF19pA",
+  authDomain: "reactjs-finance-tracker.firebaseapp.com",
+  projectId: "reactjs-finance-tracker",
+  storageBucket: "reactjs-finance-tracker.appspot.com",
+  messagingSenderId: "572525519534",
+  appId: "1:572525519534:web:c125fa92170f1e31a8833f",
+};
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Init Service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth };
