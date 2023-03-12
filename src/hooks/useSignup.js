e8cb353df2c2358e727eb1fc6d4a5f59d94f202e
Carl Caraan <bannedefused@gmail.com>
2023-03-12 21:12:11 +0800

    3/12/23

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -7,6 +7,26 @@ export const useSignup = () => {
 
   const signup = async (email, password, displayName) => {
     setError(null);
+    setIsPending(true);
+
+    try {
+      //signup user
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res.user);
+      if (!res) {
+        throw new Error("Could not complete signup");
+      }
+
+      // add display name to user
+      await res.user.updateProfile();
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
   };
 
   return { error, isPending, signup };
