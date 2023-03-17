da73f8eba5887abcb73da50d217c0d26e083cd55
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-17 22:37:52 +0800

    3/17/23

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,7 +4,12 @@ import React from "react";
 import styles from "./Home.module.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction List</div>
+      <div className={styles.sidebar}></div>
+    </div>
+  );
 }
 
 export default Home;
