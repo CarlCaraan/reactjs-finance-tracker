c80da2242763f993630859079413ec0d7c80957f
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-18 21:36:53 +0800

    3/18/23

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,41 @@
+import { useReducer, useEffect, useState } from "react";
+import { projectFirestore } from "../firebase/config";
+
+let initialState = {
+  document: null,
+  isPending: false,
+  error: null,
+  success: null,
+};
+
+const firestoreReducer = (state, action) => {
+  switch (action.type) {
+    case "IS_PENDING":
+      return { ...state, isPending: true };
+    default:
+      return state;
+  }
+};
+
+export const useFirestore = (collection) => {
+  const [response, dispatch] = useReducer(firestoreReducer, initialState);
+  const [isCancelled, setIsCancelled] = useState(false);
+
+  // Collection Ref
+  const ref = projectFirestore.collection(collection);
+
+  // add a document
+  const addDocument = (doc) => {
+    dispatch({ type: "IS_PENDING" });
+  };
+
+  // delete a document
+  const deleteDocument = (id) => {};
+
+  // Cleanup Function
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { addDocument, deleteDocument, response };
+};
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,15 @@ import React from "react";
 
 // Styles
 import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
 
 function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>Transaction List</div>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
   );
 }
