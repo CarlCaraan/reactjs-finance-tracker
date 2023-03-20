229edc93a703d891af5eb16671f9f88a7812873e
Carl Caraan <bannedefused@gmail.com>
2023-03-20 10:12:05 +0800

    Transaction Lists

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,17 +2,25 @@ import React from "react";
 
 // Styles
 import styles from "./Home.module.css";
+
+// Components
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 // Context Api
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 
 function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions");
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Transaction List</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
